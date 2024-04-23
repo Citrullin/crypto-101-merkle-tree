0685e33f07f0d505a230ed8dd209bc0436112cf5
Philipp Blum <philipp-blum@desertmonitor.com>
2024-04-23 22:09:19 +0200

    Add Unit tests and refactor to make it testable

--- src/auth/MerkleTree.js
+++ src/auth/MerkleTree.js
@@ -1,29 +1,51 @@
 const sha256 = require('sha256');
 
-module.exports = MerkleTree = {
-    verifyLeafs: (merkleTree, rootHash, leafs, elements) => {
+class MerkleTree {
+    
+    getMerkleTree(subTree){
+        //We always need to pair 2 values and create one hash from it. So it has to the power of two.
+        //If only value, we practically carry the value upwards until we can combine it.
+        function isPowerOfTwo(n) {
+            return n > 0 && (n & (n - 1)) === 0;
+        }
+        
+        function fillToPowerOfTwo(toFill) {
+            while (!isPowerOfTwo(toFill.length)) {
+                toFill.push(sha256(''));
+            }
+            return toFill;
+        }
+        
+        // One element = root hash
+        if (subTree.length === 1) {
+            return subTree[0];
+        } else {
+            const pairs = fillToPowerOfTwo(subTree).reduce((acc, _, i) => {
+                //.concat([item]) -> like push(item), but returns new array.
+                return i % 2 === 0 ? acc.concat([[subTree[i], subTree[i + 1]]]) : acc;
+            }, []).map(v => sha256(v[0] + v[1]));
+
+            return this.getMerkleTree(pairs);
+        }
+    }
+
+    verifyLeafs(rootHash, leafs, elements) {
         //We don't necessarily need the proof. As long as we have the hashed leafs, we can check ourselves.
         //Aka creating the proof ourselves, this implementation is untested.
-        return merkleTree(leafs) === rootHash && elements.map(e => leafs.indexOf(sha256(e)) > -1).every(e => e);
-    },
-    verifyProof: (merkleTree, rootHash, proof, elements) => {
-        console.log(rootHash);
-        console.log(proof);
-        console.log(elements);
+        return this.getMerkleTree(leafs) === rootHash && elements.map(e => leafs.indexOf(e) > -1).every(e => e);
+    }
 
-        console.log(
-            merkleTree(proof.leafs) === rootHash && 
-            elements.map(e => proof.elements.map((pe) => pe.value).some(pv => e == pv)).every(b => b) &&
-            proof.elements.map(e => proof.leafs[e.pos] == e.value).every(b => b)
-        );
-
-        return merkleTree(proof.leafs) === rootHash && 
+    verifyProof(rootHash, proof, elements){
+        return this.getMerkleTree(proof.leafs) === rootHash && 
         //Check if element is in the proof
         elements.map(e => proof.elements.map((pe) => pe.value).some(pv => e == pv)).every(b => b) &&
         //Check if proof elements are in leafs
         proof.elements.map(e => proof.leafs[e.pos] == e.value).every(b => b)
-    },
-    getMerkleProof: (leafs, elements) => {
+    }
+
+    //Simply just get the position of the value in the leaf and what is removed. 
+    //Can be also done without the proof and only leafs.
+    getMerkleProof(leafs, elements){
         return {
             leafs,
             elements: elements.map((e) => {
@@ -33,32 +55,7 @@ module.exports = MerkleTree = {
                 }
             })
         }
-    },
-    merkleTree: (merkleTree, subTree) => {
-        function isPowerOfTwo(n) {
-            return n > 0 && (n & (n - 1)) === 0;
-        }
-        
-        function fillToPowerOfTwo(toFill) {
-            while (!isPowerOfTwo(toFill.length)) {
-                toFill.push('');
-            }
-            return toFill;
-        }
-        
-        //Fix weird behaviour. Maybe use Typescript to avoid those issues.
-        if (!!subTree || typeof subTree == 'undefined') {
-           return sha256(''); 
-        }else if(subTree.length === 1){
-            // One element = root hash
-            return sha256(subTree[0]);
-        } else {
-            const pairs = fillToPowerOfTwo(subTree).reduce((acc, _, i) => {
-                //.concat([item]) -> like push(item), but returns new array.
-                return i % 2 === 0 ? acc.concat([[subTree[i], subTree[i + 1]]]) : acc;
-            }, []).map(v => sha256(v[0] + v[1]));
-    
-            return merkleTree(merkleTree, pairs);
-        }
     }
-};
+}
+
+module.exports = new MerkleTree();
--- src/examples/auth.js
+++ src/examples/auth.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 //We only use it to mock-data. The user would choose their own password.
 const password = require('generate-password');
 const ecdh = require('ecdh');
-const merkleTree = require('./src/auth/MerkleTree');
+const merkleTree = require('../auth/MerkleTree');
 
 const LOG_DB = false;
 
@@ -41,6 +41,7 @@ salt3 = crypto.randomBytes(12).toString('hex');
 
 //Here is where the magic happens. Given an initial database with a root has from a merkle tree.
 //We could call it Genesis block, or whatever you want. Not necessarily related to web3.
+//You can store the whole markletree and proofs in IPFS. Definitly use salt, if you do so.
 const hashed_admin_Password = sha256(admin.paddword + admin_salt);
 const admin_data = [
     {
@@ -65,7 +66,7 @@ if(LOG_DB){
 }
 
 //Our initial merkle tree, only with the admin in it. Commit this to a DLT or something.
-const init_db_root_hash = merkleTree.merkleTree(merkleTree.merkleTree, init_db_hashed_leafs);
+const init_db_root_hash = merkleTree.getMerkleTree(init_db_hashed_leafs);
 
 console.log("Admin Pub Key: ", admin_pair.publicKey.buffer.toString('hex'));
 console.log("Frontend Server Pub Key: ", frontend_server_pair.publicKey.buffer.toString('hex'));
@@ -82,7 +83,7 @@ if(LOG_DB){
 
 //Server: Verify if proof is correct, and in fact data is in the merkleproof
 const admin_pub_key = admin.pub.buffer.toString('hex');
-const isPubKeyInTree = merkleTree.verifyProof(merkleTree.merkleTree, init_db_root_hash, merkle_proof_admin_pub, [sha256(admin_pub_key + admin_salt)]);
+const isPubKeyInTree = merkleTree.verifyProof(init_db_root_hash, merkle_proof_admin_pub, [sha256(admin_pub_key + admin_salt)]);
 console.log("Is Admin key in merkle tree:", isPubKeyInTree);
 
 //Server negotiate a shared key with admin user. e.g. TLS
@@ -94,7 +95,7 @@ console.log("Admin user and Server shared key:", admin_user_shared_key.toString(
 //Let's check, if the person behind the machine also knows the password.
 //To improve security: Use pepper for passwords as well. Just one byte is enough.
 const merkle_proof_admin_pass = merkleTree.getMerkleProof(init_db_hashed_leafs, [sha256(hashed_admin_Password + admin_salt)]);
-const isPasswordInTree = merkleTree.verifyProof(merkleTree.merkleTree, init_db_root_hash, merkle_proof_admin_pass, [sha256(hashed_admin_Password + admin_salt)]);
+const isPasswordInTree = merkleTree.verifyProof(init_db_root_hash, merkle_proof_admin_pass, [sha256(hashed_admin_Password + admin_salt)]);
 
 //If we the password is in the database, we could create a session token for the admin user.
 //Something like OAuth2 etc.
@@ -102,6 +103,7 @@ console.log("Is Admin password in merkle tree:", isPasswordInTree);
 
 //The admin is going to add three new users to the database and merkle tree
 //If we would use BLS curves, we could aggregate our public keys and save space.
+//You would commit the changed markle tree and proofs into a system such as IPFS. Don't forget to use the salts.
 const to_add = [
     {
         salt: salt1,
@@ -136,7 +138,7 @@ const added_user_db = init_db_hashed_leafs.concat(
     to_add.map(obj => Object.values(obj))
     .map((item) => item.map(v => sha256(v + item[0]) )).flat()
 );
-const added_user_root_hash = merkleTree.merkleTree(merkleTree.merkleTree, added_user_db);
+const added_user_root_hash = merkleTree.getMerkleTree(added_user_db);
 
 //Not ideal since empty spaces get commited as well. Diffs/Proofs can be written more efficiently.
 if(LOG_DB){
@@ -147,14 +149,14 @@ if(LOG_DB){
 const user1_pub_key = user_1.pub.buffer.toString('hex');
 const user2_pub_key = user_2.pub.buffer.toString('hex');
 const merkle_proof_added_user = merkleTree.getMerkleProof(added_user_db, [sha256(admin_pub_key + admin_salt), sha256(user1_pub_key + salt1), sha256(user2_pub_key + salt2)]);
-const isAdminPubInAddedUser = merkleTree.verifyProof(merkleTree.merkleTree, added_user_root_hash, merkle_proof_added_user, [sha256(admin_pub_key + admin_salt)]);
+const isAdminPubInAddedUser = merkleTree.verifyProof(added_user_root_hash, merkle_proof_added_user, [sha256(admin_pub_key + admin_salt)]);
 
 //You would write the added_user_root_hash into some DLT etc. it's the updated state with new users added
 //Of course, you can have smart contract verifying the admin and change.
 console.log('Is Admin User Pub key in added user DB:', isAdminPubInAddedUser);
 
-const isUserTwoInDb = merkleTree.verifyProof(merkleTree.merkleTree, added_user_root_hash, merkle_proof_added_user, [sha256(user2_pub_key + salt2)]);
-const isUserTwoInOldDb = merkleTree.verifyProof(merkleTree.merkleTree, init_db_root_hash, merkle_proof_added_user, [sha256(user2_pub_key + salt2)]);
+const isUserTwoInDb = merkleTree.verifyProof(added_user_root_hash, merkle_proof_added_user, [sha256(user2_pub_key + salt2)]);
+const isUserTwoInOldDb = merkleTree.verifyProof(init_db_root_hash, merkle_proof_added_user, [sha256(user2_pub_key + salt2)]);
 
 //Now the new users are available as well. They can open sessions, just as the admin did.
 console.log('Is User 2 in Added User DB', isUserTwoInDb);
